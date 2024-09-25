30c3c0e6b14c9f43db2d86fd813669ea902ec472
Priyanshu <Priyanshurahate@gmail.com>
2024-09-25 12:57:47 +0530

    Part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useRef, useEffect, useState} from 'react'
+import gsap from 'gsap'
+import "./App.css"
 
-function App() {
+const App = () => {
+
+  const [direction, setDirection] = useState(1)
+   const position = direction<0 ? '0%' : '-131%';
+   
+
+  const stripe = useRef(null);
+  useEffect( () => {
+
+    gsap.to(stripe.current,{
+      x:`${50 * direction}%`,
+      duration:10,
+      repeat:-1,
+      ease:'linear',
+
+    })
+
+  },[])
+
+  // window.addEventListener('scroll',()=> {
+  //   let currentscroll = window.scrollY + 1;
+  //   let current = window.scrollY -1;
+  //   if(currentscroll > currentscroll)
+  //   {
+  //     direction=1;
+  //   }
+  //   else{
+  //     direction=-1;
+  //   }
+  //   console.log(currentscroll,window.scrollY,current);
+  // })
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+
+      <div className="page1">
+        <div ref={stripe} className='stripe' style={{left:position}}>
+          <h1>- dennis snellenberg </h1>
+          <h1>- dennis snellenberg </h1>
+        </div>
+      </div>
+
+      <div className='page2'>
+
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
